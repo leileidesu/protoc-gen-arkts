025b3e12518a9bcc797c96f12af1cd1ae53c1f60
xiao7 <3499475017@qq.com>
2024-06-27 13:43:13 +0800

    fix

--- src/common/message.rs
+++ src/common/message.rs
@@ -191,9 +191,7 @@ where
         // members.push(self.print_unknown_fields());
 
         for member in self.field.clone() {
-            if member.has_oneof_index() &&  member.proto3_optional(){
                 members.push(member.print_prop(ctx, runtime));
-            }
             if member.has_oneof_index() && ! member.proto3_optional(){
                 let other_oneofs = self.get_oneof_fields(&member);
                 members.push(member.print_oneof_getter(ctx, runtime));
